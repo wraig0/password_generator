9fa9d761a5f8a8eeba3b0950f6d7fcb4616f90ea
Thomas Underwood <37879441+wraig0@users.noreply.github.com>
2024-05-22 22:58:56 +0100

    node version of password generator

---
+++ password_generator.js
@@ -0,0 +1,17 @@
+const crypto = require("crypto");
+
+const generateRandomString = (length) => {
+  const characters =
+    " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"£$%^&*()[]{}_-+=;:'@#><,./?\\";
+  let randomString = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = crypto.randomInt(0, characters.length);
+    randomString += characters[randomIndex];
+  }
+
+  return randomString;
+};
+
+const randomString = generateRandomString(64);
+console.log(randomString);
